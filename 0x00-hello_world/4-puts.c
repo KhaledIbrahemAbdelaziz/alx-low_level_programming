f96be60a2304a1c6ddf5f6c3e61b5b49387bdd11
Your Name <you@example.com>
2023-02-16 12:29:13 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * mian - Entry point
+ * Return: always 0 (success)
+ */
+int main(avoid){
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
