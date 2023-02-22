d2d630ca1522e3e76cbfafa4829230966dd7af32
Your Name <you@example.com>
2023-02-21 19:30:53 -0800

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - Adds two integers 
+ * @n1: first int
+ * @n2: second int
+ * Return: the result of the summition of n1 and n2
+ */
+
+int add(int n1, int n2)
+{
+	int result;
+
+	result = n1 + n2;
+	return (result);
+}
