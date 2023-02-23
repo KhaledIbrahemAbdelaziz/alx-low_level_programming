c07114f3ba160eaa0eefbc99e439431eab1e4eea
Your Name <you@example.com>
2023-02-23 09:01:57 -0800

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints the numbers from 0 to 14 10 times
+ *
+ * Return: nothing to return
+ */
+
+void more_numbers(void)
+{
+	int i = 0;
+
+	int j;
+
+	while (i < 10)
+	{
+		for (j = 0; j < 15; j++)
+			_putchar(j + '0');
+		_putchar('\n');
+		i++;
+	}
+	_putchar('\n');
+}
