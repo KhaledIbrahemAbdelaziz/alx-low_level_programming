1778a167994cf0e189e4985bdc839a2085bef9c7
Your Name <you@example.com>
2023-02-23 11:05:16 -0800

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -18,8 +19,6 @@ int main(void)
 			printf("Buzz ");
 		else if ((i % 3) == 0 && (i % 5) == 0)
 			printf("FizzBuzz ");
-		else if (i == 1)
-			printf("%d ", i);
 		else
 			printf("%d ", i);
 	}
