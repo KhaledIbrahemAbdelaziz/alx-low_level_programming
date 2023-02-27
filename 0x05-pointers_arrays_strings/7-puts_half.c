91feea230966630c83eb7e654e20bb1ea2cd4c79
Your Name <you@example.com>
2023-02-27 12:30:35 -0800

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - prints half of a string, followed by a new line
+ * @str: The char pointer for the string
+ * Return: nothing to return
+ */
+
+void puts_half(char *str)
+{
+	int len1, len2;
+
+	len1 = strlen(str);
+	if (len1 % 2 == 1)
+		len2 = len1 / 2 + 1;
+	else
+		len2 = len1 / 2;
+
+	for (int i = len2; i < len1; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
