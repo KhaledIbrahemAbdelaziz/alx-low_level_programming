8733be7fc2681e1f8fb87707bdd69585e2a6860b
Your Name <you@example.com>
2023-02-27 11:43:58 -0800

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,19 +1,20 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * print_rev - prints a string, in reverse, followed by a new line.
  * @s: The char pointer for the string
  * Return: nothing to return
  */
+
 void print_rev(char *s)
 {
-	int len;
+	int len, i;
 
 	len = _strlen(s);
-	while (*s != '\0')
+	for (i = len - 1; i >= 0; i++)
 	{
-		_putchar(*s);
-		s--;
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
