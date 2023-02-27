afcbc890d58bd27150408cd10751373dace97c69
Your Name <you@example.com>
2023-02-27 13:27:36 -0800

    Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -16,12 +16,14 @@ char *_strcpy(char *dest, char *src)
 
 	len1 = 0;
 	len2 = 0;
-	while (*src != '\0')
+	while (*(src + len1) != '\0')
 		len1++;
+
 	for (; len2 < len1; len2++)
 	{
 		dest[len2] = src[len2];
 	}
+
 	dest[len1] = '\0';
 	return (dest);
 }
