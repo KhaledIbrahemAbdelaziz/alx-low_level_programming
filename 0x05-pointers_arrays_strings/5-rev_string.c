464951019db53a977363957c6d1d4d63a2714610
Your Name <you@example.com>
2023-02-27 12:07:29 -0800

    Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,12 +9,21 @@
 
 void rev_string(char *s)
 {
-	int len;
+	char temp;
 
-	len = _strlen(s);
-	while (s[len - 1] != '\0')
+	int len1, len2, i;
+
+	len1 = 0;
+	len2 = 0;
+	while (s[len1] != '\0')
+		len1++;
+
+	len2 = len1 - 1;
+	for (i = 0; i < len1 / 2; i++)
 	{
-		_putchar(s[len - 1]);
-		len--;
+		temp = s[i];
+		s[i] = s[len2];
+		s[len2] = temp;
+		len2--;
 	}
 }
