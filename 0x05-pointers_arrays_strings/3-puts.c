bc08baa522aeb1826b0c5e562b68534438a6ca25
Your Name <you@example.com>
2023-02-27 11:22:26 -0800

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: The char pointer for string
+ * Return: nothing to return
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		printf("%c", *str);
+		str++;
+	}
+	printf("\n");
+}
