47e22be7ad2eb1aa61b52501b7e8abf66a8d7bf6
Your Name <you@example.com>
2023-03-08 12:47:32 -0800

    Task 0

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _putchar - Prints the characters we want
+ * @c: The character to be printed
+ * 
+ * Return: 1 On success
+ * On error -1 and errno is set appropriately
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
