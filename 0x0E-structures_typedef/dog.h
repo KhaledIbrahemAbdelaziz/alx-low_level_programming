24c0aaca2cbee48aefd4f68294090d83aae93f0c
Your Name <you@example.com>
2023-03-20 17:43:56 -0700

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,17 +8,16 @@
  * @owner: Owner.
  * Description: This struct is for dog.
  */
-
-struct dog {
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
 
 /**
- * dog_t - typedef for struct dog
+ * dog_t - typedef for struct dog.
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
