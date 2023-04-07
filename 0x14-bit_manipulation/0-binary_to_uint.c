91e7ab5baff1d100da7f7d48c2306fe5892acda5
your name <you@example.com>
2023-04-06 19:34:56 -0700

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,7 +16,7 @@ unsigned int binary_to_uint(const char *b)
 	i = 0;
 	while (b[i])
 	{
-		if (b[i] != '0' || b[i] != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
 		num = (b[i] - '0') + 2 * num;
 		i++;
